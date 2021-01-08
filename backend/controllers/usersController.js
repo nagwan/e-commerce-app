6d74942beb0a903d924537776b86bc6a16b723c5
nagwan sami <nagwan.s.abdelhamid@gmail.com>
2021-01-08 22:45:52 +0200

    migrate to use controllers

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,7 @@
+import asyncHandler from "express-async-handler"
+import User from "../models/User.js"
+
+
+const login = asyncHandler(async (req, res) => {
+
+})
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,6 +1,6 @@
 import express from "express"
 const router = express.Router()
-import {index, view} from "../controllers/products.js"
+import {index, view} from "../controllers/productsController.js"
 
 router.get('/', index)
 
