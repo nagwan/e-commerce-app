b3434497c36a48a3514cf3e3605ed2f307341404
nagwan sami <nagwan.s.abdelhamid@gmail.com>
2021-01-09 15:18:25 +0200

    input field componant

---
+++ frontend/src/components/partials/InputField/InputField.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./inputField.sass"
+
+export default function InputField({ type, value, error, handleChange, placeholder }) {
+  return (
+    <div className="w-100">
+      <input className="radius-6 p-5 fs-16 font-primary-regular" type={type} placeholder={placeholder} value={value} onChange={e => handleChange(e.target.value)} />
+      {error && <span className="txt-color-danger-500 fs-12 font-primary-regular">{error}</span>}
+    </div>
+  )
+}
+
