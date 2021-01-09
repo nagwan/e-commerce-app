5f4925ec0f22ad4e713503726815c5c414a6a878
nagwan sami <nagwan.s.abdelhamid@gmail.com>
2021-01-09 17:43:24 +0200

    navbar enhancement

--- frontend/src/components/partials/Navbar/Navbar.js
+++ frontend/src/components/partials/Navbar/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./navbar.sass"
+import { useSelector } from "react-redux"
 
 export default function Navbar() {
+    const user = useSelector(state => state.user.user)
     return (
         <nav className="w-100 shadow-2 bg-main-800 h-80px d-flex justify-content-between align-items-center txt-color-white p-x-15">
             <Link to="/" className="txt-color-white no-decoration">
@@ -17,10 +19,15 @@ export default function Navbar() {
                     </div>
                 </Link>
 
-                <div>
-                    <i className="far fa-user"></i>
-                    <span className="m-x-5">My Account</span>
-                </div>
+                <Link to={user.id ? '/me' : '/login'} className="m-x-15 txt-color-white no-decoration">
+                    <div>
+                        <i className="far fa-user"></i>
+                        <span className="m-x-5">{user.id ? user.name : 'Login'}</span>
+
+                    </div>
+                </Link>
+
+
             </div>
 
         </nav>
